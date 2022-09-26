a2b559be20ed82469bb897ec74c913f518917f5e
Selena <selena.garciamerino@cieep.com>
2022-09-26 20:41:29 +0200

    Escritura y lectura de Binarios

---
+++ src/ficheros_binarios/LeerBinarios.java
@@ -0,0 +1,27 @@
+package ficheros_binarios;
+
+import java.io.*;
+
+public class LeerBinarios {
+    public static void main(String[] args) {
+        File file = new File("binarios.dat");
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            DataInputStream dis = new DataInputStream(fis);
+
+            //Creamos una excepción porque no sabemos cuan largo es el archivo, añadiremos el catch de eofException
+            while(true){
+                int numero =dis.readInt();
+                System.out.printf("El número es: %d%n", numero);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        catch (EOFException eofException){
+            System.out.println("Fichero terminado");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
