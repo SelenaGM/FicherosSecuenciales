6c3cea597dcb75170c343abd1b64a03b6d85a895
Selena <selena.garciamerino@cieep.com>
2022-09-29 20:34:52 +0200

    Objetos en ficheros

---
+++ src/ficheros_objetos/Dado.java
@@ -0,0 +1,22 @@
+package ficheros_objetos;
+
+import java.io.Serializable;
+
+public class Dado implements Serializable {
+    public static final int CARAS = 6;
+
+    private int tirada;
+
+    public Dado(int tirada) {
+        this.tirada = tirada;
+    }
+
+    public int getTirada() {
+        return tirada;
+    }
+
+    public void setTirada(int tirada) {
+        this.tirada = tirada;
+    }
+
+}
---
+++ src/ficheros_objetos/Main.java
@@ -0,0 +1,73 @@
+package ficheros_objetos;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Main {
+
+    private static ArrayList<Dado> dados;
+    private static File dadosFichero;
+
+    static{
+        dados = new ArrayList<>();
+        dadosFichero = new File("dados.dat");
+    }
+
+    public static void main(String[] args) {
+
+        crearTiradas();
+        escribirTiradas();
+        leerTiradas();
+
+    }
+
+    private static void leerTiradas() {
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(dadosFichero);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            while(true){
+                Dado dado = (Dado)ois.readObject();
+                System.out.printf("La tirada es: %d%n",dado.getTirada());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }catch(EOFException e){
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void escribirTiradas() {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(dadosFichero);
+            ObjectOutputStream ous = new ObjectOutputStream(fos);
+            for (Dado dado:dados) {
+                ous.writeObject(dado);
+            }
+            ous.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void crearTiradas() {
+        for (int i = 0; i < 1000; i++) {
+            dados.add(new Dado((int)(Math.random() * Dado.CARAS + 1)));
+        }
+    }
+}
